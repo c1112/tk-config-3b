4c668e5d58ad443e37a0f8891463629eec6c390f
c1112 <bfitzgerald011@gmail.com>
2022-01-25 16:13:50 -0500

    selection set alembic publishing from maya
    Initial implementation of alembic publishing from maya using selection sets to decide what gets published.  "_abc" is the token that is looked for in the selection set name.  bunch of cleanup and edge cases to protect for. also would like to inherit from collector.py into abc_collector.py to avoid duplication of code.

--- hooks/tk-multi-publish2/maya/abc_collector.py
+++ hooks/tk-multi-publish2/maya/abc_collector.py
@@ -106,6 +106,21 @@ class MayaSessionCollector(HookBaseClass):
                 },
             )
 
+        #grab the entire contents of the scene
+        scenecontents = cmds.ls(noIntermediate=True)
+        #selection set filter
+        self.ssmatch = '_abc'
+        #selection sets that match the filter
+        ssets = []
+
+        #lookfor and append selection sets that match the filter to our list
+        for node in scenecontents:
+            if cmds.nodeType(node) == 'objectSet' and node.endswith(self.ssmatch):
+                ssets.append(node)
+
+        if self.ssmatch:
+            self._collect_selectionset_geometry(item, ssets)
+
     def collect_current_maya_session(self, settings, parent_item):
         """
         Creates an item that represents the current maya session.
@@ -161,3 +176,38 @@ class MayaSessionCollector(HookBaseClass):
         self.logger.info("Collected current Maya scene")
 
         return session_item
+
+    def _collect_selectionset_geometry(self, parent_item, ssets):
+        """
+        Creates items for session geometry to be exported.
+
+        :param parent_item: Parent Item instance
+        """
+        #loop through and create an item for each selection set
+        for set in ssets:
+            # check from namespace if namespace exsists use the trailing number
+            # in it to difer it.  sorta a hack. this works on 1-9 but falls flat
+            # after 9 and only if someone doesn't monkey with name spaces
+            nspace = set.split(":")
+            if len(nspace) <= 1:
+                setname = set.replace(self.ssmatch, '')
+            else:
+                setname = nspace[1].replace(self.ssmatch, '')
+                # check to see if there is digit at the end of the end of the
+                # namespace and use that differ the publish
+                refnum = nspace[0][-1:]
+                if refnum.isdigit():
+                    setname = "%s%s" % (setname, refnum)
+
+            geo_item = parent_item.create_item(
+                "maya.session.geometry", "Selection Set", setname
+            )
+
+            #create a property to store all the set members to pass to publishing
+            cmds.select(set)
+            geo_item.properties['setmembers'] = cmds.ls(sl=True, l=True)
+
+            # get the icon path to display for this item
+            icon_path = os.path.join(self.disk_location, os.pardir, "icons", "geometry.png")
+
+            geo_item.set_icon_from_path(icon_path)
